9297405ad4cae4bc4497a801fcb9fa41d7867399
Milana <milana.stojadinov@gmail.com>
2018-04-27 20:16:44 +0200

    Add mobile events

--- assets/game_of_life.js
+++ assets/game_of_life.js
@@ -1,13 +1,13 @@
 var w, columns, rows;
 var insert_mode, clear_mode, easter_egg, pause_mode;
-var counter, frequence;
+var counter, frequence, touch_count;
 let board, next;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   w = 8;
   easter_egg = 0;
-  counter = 0; frequence = 50;
+  counter = 0; frequence = 50; touch_count = 0;
   insert_mode = 0; clear_mode = 0; pause_mode = 0;
   columns = floor(width/w);
   rows = floor(height/w);
@@ -110,6 +110,26 @@ function keyTyped(){
     };
   } else randomize_colors();
 
+  return false;
+}
+
+function touchStarted() {
+  touch_count += 1;
+  if (touch_count % 2 === 0){
+    pause_mode = 1;
+  }
+  else {
+    pause_mode = 0;
+    frequence = 50;
+    easter_egg = 1;
+    randomize_colors();
+    board.empty();
+    board.fill(0, columns, rows - 30, rows);
+  }
+  return false;
+}
+
+function touchMoved() {
   return false;
 }
 
